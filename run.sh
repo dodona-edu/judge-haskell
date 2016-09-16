c6ee6bda9367b1cf5d29bb108be2bcd14b5c4372
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-09-16 17:23:35 +0200

    fix syntax error

--- run.sh
+++ run.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
 input="$(cat | base64)"
+env="$(ls | base64)"
 
 cat <<HERE
 {
+    "accepted": false,
+    "description": "failed :-(",
+    "status": "helaas onsuccesvol",
+    "messages": [{
+        "format": "code",
+        "description": "$input"
+    },{
+        "format": "code",
+        "description": "$env"
+    }],
     "groups": [{
+        "accepted": false,
         "description": "tab title",
-        "messages": [{
-            "format": "code",
-            "description": "$input"
-        }],
         "groups": [{
             "accepted": false,
             "groups": [{
                 "accepted": false,
                 "description": "ggd 12 3",
-                "generated": "4",
-                "expected": "3"
+                "tests": [{
+                    "accepted": false,
+                    "generated": "4",
+                    "expected": "3"
+                }]
             }]
         },{
             "accepted": true,
             "groups": [{
                 "accepted": true,
                 "description": "ggd 12 4",
-                "generated": "4"
+                "tests": [{
+                    "accepted": true,
+                    "generated": "4"
+                }]
             }]
         }]
     }]
