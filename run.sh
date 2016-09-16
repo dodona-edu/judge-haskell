16aecc91c5e51785690ae72f39e0e506de5c7f97
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-09-16 17:02:20 +0200

    try other format and use input as output

--- run.sh
+++ run.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
+input="$(cat | base64)"
+
 cat <<HERE
 {
     "groups": [{
         "description": "tab title",
         "messages": [{
             "format": "code",
-            "description": "asdf\\nasdf\\nasdf"
+            "description": "$input"
         }],
         "groups": [{
             "accepted": false,
             "groups": [{
                 "accepted": false,
-                "groups": [{
-                    "accepted": false,
-                    "decription": "ggd 12 3",
-                    "generated": "4",
-                    "expected": "3"
-                }]
+                "description": "ggd 12 3",
+                "generated": "4",
+                "expected": "3"
             }]
         },{
             "accepted": true,
             "groups": [{
                 "accepted": true,
-                "groups": [{
-                    "accepted": true,
-                    "decription": "ggd 12 4",
-                    "generated": "4"
-                }]
+                "description": "ggd 12 4",
+                "generated": "4"
             }]
         }]
     }]
