6a7e5e96c7474777c2601c2ce396cbb9e99d26d1
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-09-16 15:37:43 +0200

    add dummy run.sh

---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cat <<HERE
+{
+    "groups": [{
+        "description": "tab title",
+        "messages": [{
+            "format": "code",
+            "content": "asdf\\nasdf\\nasdf"
+        }],
+        "groups": [{
+            "accepted": false,
+            "groups": [{
+                "accepted": false,
+                "groups": [{
+                    "accepted": false,
+                    "decription": "ggd 12 3",
+                    "generated": "4",
+                    "expected": "3"
+                }]
+            }]
+        },{
+            "accepted": true,
+            "groups": [{
+                "accepted": true,
+                "groups": [{
+                    "accepted": true,
+                    "decription": "ggd 12 4",
+                    "generated": "4",
+                }]
+            }]
+        }]
+    }]
+}
+HERE
+
