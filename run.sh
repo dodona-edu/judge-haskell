d5cf2a12010c6317ab75d534542a1ba8f5bc6c40
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-09-19 17:44:25 +0200

    run all test files

--- run.sh
+++ run.sh
@@ -6,23 +6,34 @@ RESOURCES=$(jsawk  'return this.resources' <<< "$VALUE")
 touch "Input.hs"
 echo "module Input where" >  "Input.hs"
 cat "$SOURCE" >>  "Input.hs" 
-cp  $(echo "$RESOURCES"/*_test.hs)  . 
-output="$(runghc *_test.hs 2> stderr.file)"
-errput="$(cat stderr.file | base64 | tr -d '\n')"
 
 cat <<HERE
 {
     "accepted": false,
     "description": "failed :-(",
     "status": "wrong",
-    "messages": [{
-        "description": "$errput",
-        "format": "code"
-    }],
     "groups": [{
+HERE
+
+find "$RESOURCES" -name '*_test.hs' | while read testfile; do
+    #cp  $(echo "$RESOURCES"/*_test.hs)  . 
+    testname="${testfile%_test.hs}"
+    output="$(runghc -i.:"$RESOURCES" "$testfile" 2> stderr.file)"
+    errput="$(cat stderr.file | base64 | tr -d '\n')"
+
+    cat <<HERE
+    }, {
         "accepted": false,
-        "description": "tab title",
+        "description": "${testname//_/ }",
+        "messages": [{
+            "description": "$errput",
+            "format": "code"
+        }],
         "groups": ${output:-[]}
+HERE
+done | tail -n +2
+
+cat <<HERE
     }]
 }
 HERE
