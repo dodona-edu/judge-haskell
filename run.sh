fc9f0eb1daa5e208e583eb189caa77fbc524a64d
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-09-20 16:42:34 +0200

    add workdir to haskell judge

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-VALUE=$(cat)
-SOURCE=$(jq -r '.source' <<< "$VALUE")
-RESOURCES=$(jq -r '.resources' <<< "$VALUE")
-JUDGE=$(jq -r '.judge' <<< "$VALUE")
 
-touch "Input.hs"
-echo "module Input where" >  "Input.hs"
-cat "$SOURCE" >>  "Input.hs" 
+VALUE="$(cat)"
+SOURCE="$(jq -r '.source' <<< "$VALUE")"
+RESOURCES="$(jq -r '.resources' <<< "$VALUE")"
+JUDGE="$(jq -r '.judge' <<< "$VALUE")"
+WORKDIR="$(jq -r '.workdir' <<< "$VALUE")"
+
+cd "$WORKDIR"
+
+# Create the Input.hs module, containing the submitted code.
+echo "module Input where" > "Input.hs"
+cat "$SOURCE" >> "Input.hs" 
 
 cat <<HERE
 {
@@ -22,7 +26,6 @@ find "$RESOURCES" -name '*_test.hs' | while read testfile; do
     testfile="$(basename "$testfile")"
     testname="${testfile%_test.hs}"
     tabtitle="${testname//_/ }"
-    #output="$(runghc -i.:"$RESOURCES" "$testfile" 2> stderr.file)"
     output="$(cabal exec runghc "$testfile" 2> stderr.file)"
 
     cat <<HERE
